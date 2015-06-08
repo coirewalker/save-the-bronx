db210f7b1a257049eaac807429ad9003fc31e161
whereiscoire <whereiscoire@gmail.com>
2015-06-08 10:03:50 -0400

    added interactive Gmap on Help page

--- js/script.js
+++ js/script.js
@@ -6,85 +6,71 @@ $(document).ready(function() {
     opacity: 1.0,
     width: (bgWidth * 1.1),
   }, 8000);
- 	$("p, h1, h2, article").hide().fadeIn(8000);
 
- // $(".statement-home")window.setTimeout.hide().fadeIn, 10000));
+ 	$("p, h1, h2, article").hide().fadeIn(8000);
+  $("#googleMap").hide().fadeIn(8000);
 
 	function fadeStatement() {
-	  $(".statement-home, .statement, .statement-donate").hide().delay(4500).fadeIn(2000);
+	  $(".statement-home, .statement, .statement-donate").hide().delay(4500).fadeIn(3000);
+    $(".donate-bg-fade-delay").hide().delay(10).fadeIn(3500);
 	};
 
-	fadeStatement();
-
-	// function delayStache() {
-	//   $(".stache").delay(2000);
-	// };
-
-	// delayStache();
- 
+	fadeStatement(); 
 
 
 
-//////////////// GMAP ///////////////
+  //////////////// GMAP //////////////
 
-	var map = new GMaps({
-    el: '#map',
-    lat: 40.8447819,
-    lng: -73.8648268
+
+
+  var map
+  var myCenter=new google.maps.LatLng(40.8447819,-73.8648268);
+
+  function initialize() {
+    var mapProp = {
+      center:myCenter,
+      zoom:13,
+      mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+    google.maps.event.addListener(map, 'click', function(event) {
+      placeMarker(event.latLng);
+    });
+  }
+
+  function placeMarker(location) {
+    
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      icon: '../img/hipster-icon.png',
+       map:map,
+    draggable:true,
+    animation: google.maps.Animation.BOUNCE,
+  
+    });
+
+    marker.setMap(map);
+
+    var infowindow = new google.maps.InfoWindow({
+      content: '<span style="font-size: 1.5em; font-weight: bold; background-color: none;">Hipster Sighting!!!  RED ALERT!!!  HSWAT DISPATCH!!!</span>' + '<br>Latitude: ' + location.lat() + '<br>Longitude: ' + location.lng()
+    });
+    infowindow.open(map,marker);
+  }
+
+  $('.map-alarm-div').on('click', function(){
+    $('.map-alarm-div').addClass('alarm-box');
+    $('.map-lower-para').addClass('alarm-bg');
   });
 
-  // map.addMarker({
-  //   lat: 40.8484179,
-  //   lng: -73.8625094,
-  //   title: 'hipster sighting',
-  //   click: function(e) {
-  //     alert('You clicked in this marker');
-  //   }
-  // });
+  google.maps.event.addDomListener(window, 'load', initialize);
 
-  // map.addMarker({
-  //   lat: 40.8404964,
-  //   lng: -73.8489481,
-  //   title: 'hipster sighting',
-  //   click: function(e) {
-  //     alert('You clicked in this marker');
-  //   } 
-  // });
-
-  // map.addMarker({
-  //   lat: 40.8375743,
-  //   lng:-73.8610502,
-  //   title: 'hipster sighting',
-  //   click: function(e) {
-  //     alert('You clicked in this marker');
-  //   }
-  // });
-
-  // map.addMarker({
-  //   lat: 40.8297165,  
-  //   lng: -73.8449141,
-  //   title: 'hipster sighting',
-  //   click: function(e) {
-  //     alert('You clicked in this marker');
-  //   }
-  // });
-/////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/////////////
 
 
 
 });
+
+
